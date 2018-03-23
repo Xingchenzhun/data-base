e6db4c75917f923f25e2f38c706404fe97b6a31c
Xingchenzhun <34294210+Xingchenzhun@users.noreply.github.com>
2018-03-23 16:12:00 +0800

    Add files via upload

---
+++ cw/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text.Trim();  //取出账号
+            string password = textBox2.Text.Trim();  //取出密码
+
+            //string connstr = ConfigurationManager.ConnectionStrings["connectionString"].ToString(); //读取连接字符串
+            string myConnString = "Data Source=.;Initial Catalog=cw;Integrated Security=True";
+
+            SqlConnection sqlConnection = new SqlConnection(myConnString);  //实例化连接对象
+            sqlConnection.Open();
+
+            string sql = "select id,password from LoginManage where id = '" + username + "' and password = '" + password + "'";                                            //编写SQL命令
+            SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            if (sqlDataReader.HasRows)
+            {
+                MessageBox.Show("恭喜，登录成功！");
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+
+            else
+                MessageBox.Show("登陆失败,请重新输入");
+            sqlConnection.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
